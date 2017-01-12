c7406420e591ca3b7406fdee83dabadb2199aafc
Andrew James Kinsey <akinsey@andrews-macbook-air-123.local>
2017-01-12 10:27:20 -0600

    Initial Commit

---
+++ animationEdumacation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  animationEdumacation
+//
+//  Created by Andrew James Kinsey on 1/12/17.
+//  Copyright © 2017 The Cabinents. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
